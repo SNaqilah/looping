de0ae1241002c5b2de8a51fc9519a6c30aa57daa
user <user@LAPTOP-JAQA1I9I>
2020-11-26 21:28:53 +0800

    quiz2

---
+++ week7/src/week7/Quiz.java
@@ -0,0 +1,111 @@
+package week7;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WHILE
+		
+		// 1.
+		System.out.println("1.");
+		int max = 5;
+		for (int n = 1; n <= max; n++) {
+		    System.out.println(n);
+		}
+		System.out.println();
+	
+	}//answer 1
+	{	System.out.println("\n 1. ");
+		int max=5;
+	 int n =1;
+			while (n<=max) {
+				System.out.print(n);
+			n++;}	
+			
+	{
+		//NO 2
+		System.out.println("2.");
+		int total = 25;
+		for(int number = 1; number <= (total / 2); number++) {
+		    total = total - number;
+		    System.out.println(total + " " + number);
+		}
+		
+		
+	}//answer 2
+	{System.out.println("\n 2. ");
+	int total=25;
+	int number =1;
+		while (number<=(total / 2)) {
+		System.out.print(total + " " + number);
+		number++;}
+	}
+	
+	
+	
+	//NO 3
+	{	System.out.println("3.");
+		for (int i = 1; i <= 2; i++) {                                     
+		    for (int j = 1; j <= 3; j++) {
+		        for (int k = 1; k <= 4; k++) {
+		            System.out.print("*");
+		        }
+		        System.out.print("!");
+		    }
+		    System.out.println();
+		}
+		System.out.println();
+	
+	}//answer 3
+	{System.out.println("\n 3. ");
+	int i =1;
+	while (i<=2) {
+		System.out.print("*");
+		i++;}
+	{System.out.println("\n 3. ");
+	int j =1;
+	while (j<=3) {
+		System.out.print("!");
+		j++;}
+	{System.out.println("\n 3. ");
+	int k =1;
+	while (k<=4) {
+		System.out.print("%");
+		k++;}
+			
+	}
+	//NO 4.
+	System.out.println("4.");
+	int number = 4;
+	for (int count = 1; count <= number; count++) {
+	    System.out.println(number);
+	    number = number / 2;
+	}//answer 4
+	{System.out.println("\n4. ");
+	int number1 = 4;
+	int count = 1;
+	while(count <= number){
+		System.out.println("number");
+		count++;}
+			
+				
+			
+
+
+	
+
+	}
+}}}}
+	
+    	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+    
+
---
+++ week7/src/week7/WhileLoop.java
@@ -0,0 +1,74 @@
+package week7;
+
+public class WhileLoop {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for (int i =1;i<=5;i++) {
+				System.out.println("For " + i);
+			}
+			    int i=1;
+			    do {
+				System.out.println("Do While " + i);
+				i++;
+			}   while (i<=10);
+				}
+			    int a=1;{
+			    while (a<=5) {
+				System.out.println("while " + a);
+				a++;
+			}
+			    //loop of i
+			    for (int i=1;i<=3; i++) {
+			    //loop of j
+			    for(int j=1;j<3; j++) {
+			    	System.out.println("For :" +i+" " +j);{
+			    }//end of i
+			}//end of j
+			    
+			    //code of java Do
+			    int i1=1;{
+			    
+			    do {
+			    	int j=1;
+			    	do {
+			    		System.out.println("While:" +i + " " + j);
+			    		j++;
+			    	}while(j<=3);
+			    	
+			    	i++;
+			    }while(i<=3);
+			    System.out.println();
+			    	}	
+			    int i11=0;
+			    while (i<5) {
+			    	int j=0;
+			    	while(j<i) {
+			    		System.out.print("*");
+			    		j++;
+			    	}
+			    	i11++;
+			    	System.out.println();
+			    }
+			   int a=0;
+			   do {
+				   int b=0;
+				   do {
+					   System.out.print("*");
+					   b++;
+					   System.out.print("*");
+				   }while(b<a);
+				   System.out.println();
+				   }
+			   while(a<5);
+			   }
+			   
+			    	}
+			    
+					}
+					
+			    
+
+
+
